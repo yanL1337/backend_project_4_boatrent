23254d93849dabd84ad150be31e61278d9356f43
Yan Luka Keita <yanlukakeita2000@gmail.com>
2023-12-29 09:59:28 +0100

    comments

--- backend/boote/controller.js
+++ backend/boote/controller.js
@@ -5,17 +5,23 @@ import fs from "fs/promises";
 
 const DIR = "./uploads/";
 
+// Fügt ein neues Boot zur Datenbank hinzu
 export const addBoot = async (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
+  console.log(req.body); // Loggt die Anfragedaten
+  console.log(req.file); // Loggt die Dateiinformationen
 
+  // Bestimmt den Dateityp der hochgeladenen Datei und generiert einen Pfad
   const data = await fileTypeFromBuffer(req.file.buffer);
   const path = DIR + v4() + "." + data.ext;
 
+  // Speichert die Datei im Dateisystem
   await fs.writeFile(path, req.file.buffer);
 
+  // Erstellt ein neues Boot-Objekt und fügt den Link zum Bild hinzu
   const boot = new Boot(req.body);
   boot.imgLink = "https://boat-rent.onrender.com/" + path;
+
+  // Versucht, das Boot in der Datenbank zu speichern und sendet eine Antwort
   try {
     await boot.save();
     res.status(201).end();
@@ -24,22 +30,30 @@ export const addBoot = async (req, res) => {
     res.status(500).end();
   }
 };
+
+// Ruft alle Boote aus der Datenbank ab
 export const getBoote = async (req, res) => {
   const boote = await Boot.find();
   res.json(boote).end();
 };
+
+// Löscht ein spezifisches Boot aus der Datenbank
 export const delBoot = async (req, res) => {
   const response = await Boot.deleteOne({ _id: req.body.id });
   res.json(response).end();
 };
 
+// Fügt eine Reservierung zu einem bestimmten Boot hinzu
 export const addResv = async (req, res) => {
+  // Ruft Reservierungsdaten von einem externen Dienst ab
   const data = await fetch(
     "https://boat-rent.onrender.com/api/reservierung/get"
   ).then((res) => res.json());
 
+  // Filtert Reservierungen für das spezifizierte Boot
   const reservierung = data.filter((resv) => resv.Boot == req.body.Boot);
 
+  // Aktualisiert das Boot-Objekt in der Datenbank mit der neuen Reservierung
   const response = await Boot.updateOne(
     { _id: req.body.Boot },
     { $addToSet: { Reservierung: reservierung } }
@@ -47,11 +61,14 @@ export const addResv = async (req, res) => {
   res.json(response).end();
 };
 
+// Ruft Boote ab, die im angegebenen Zeitraum verfügbar sind
 export const getAviableBoote = async (req, res) => {
   try {
+    // Parst die Datumswerte aus der Anfrage
     const startDatum = new Date(req.body.start);
     const endDatum = new Date(req.body.end);
 
+    // Führt eine Abfrage aus, um Boote zu finden, die im angegebenen Zeitraum verfügbar sind
     const Boote = await Boot.find({
       Reservierung: {
         $not: {
--- frontend/src/pages/BooteAdd.jsx
+++ frontend/src/pages/BooteAdd.jsx
@@ -1,26 +1,39 @@
-import { useContext, useRef } from "react";
-import Aside from "../components/Aside";
+// Importieren notwendiger Hooks und Komponenten aus React und anderen Dateien
+import { useContext } from "react";
 import { RefreshContext } from "../components/Handler";
 
+// Die Komponente `BooteAdd` für das Hinzufügen neuer Boote
 const BooteAdd = () => {
+  // Verwendung des useContext Hooks, um auf den RefreshContext zuzugreifen
+  // Dies wird wahrscheinlich genutzt, um die Ansicht zu aktualisieren, wenn ein neues Boot hinzugefügt wird
   const refresh = useContext(RefreshContext);
 
+  // Die Funktion `addBoot` wird aufgerufen, wenn das Formular gesendet wird
   const addBoot = async (event) => {
+    // Verhindert die Standardaktion des Formulars beim Senden
     event.preventDefault();
 
+    // Erstellen eines FormData Objekts aus dem gesendeten Formular
     const form = new FormData(event.target);
 
+    // Sendet eine POST-Anfrage an den Server mit den Formulardaten
+    // Die URL des Backends wird aus den Umgebungsvariablen geladen
     await fetch(import.meta.env.VITE_BACKENDURL + "/boot/add", {
       method: "POST",
       body: form,
     });
 
+    // Aktualisiert den Zustand in `RefreshContext`, um die Anzeige zu aktualisieren
+    // Wahrscheinlich um die Liste der Boote neu zu laden
     refresh((prev) => !prev);
-    //console.log(obj);
   };
+
+  // Das Rendern des Formulars zur Eingabe der Bootsinformationen
   return (
     <div className="w-fit flex m-auto">
       <form className="flex flex-col" onSubmit={addBoot}>
+        {/* Eingabefelder für verschiedene Bootseigenschaften wie Baujahr, Seriennummer, etc. */}
+        {/* Jedes Feld verwendet Tailwind CSS Klassen für das Styling */}
         <input
           className="input input-bordered input-primary w-full max-w-xs"
           type="number"
@@ -28,45 +41,14 @@ const BooteAdd = () => {
           name="Baujahr"
           placeholder="Baujahr.."
         />
-        <input
-          className="input input-bordered input-primary w-full max-w-xs"
-          type="number"
-          id="Seriennummer"
-          name="Seriennummer"
-          placeholder="Seriennummer..."
-        />
-        <select
-          name="Material"
-          className="select select-primary w-full max-w-xs"
-          id="Material"
-        >
-          <option value="Seelen">Seelen</option>
-          <option value="GFK">GFK</option>
-          <option value="Holz">Holz</option>
-          <option value="Metall">Metall</option>
-          <option value="Pappe">Pappe</option>
-        </select>
-        <select
-          className="select select-primary w-full max-w-xs"
-          name="Bootsart"
-          id="Bootsart"
-        >
-          <option value="Tretboot">Tretboot</option>
-          <option value="Segelboot">Segelboot</option>
-          <option value="Luftkissenboot">Luftkissenboot</option>
-          <option value="Geisterschiff">Geisterschiff</option>
-          <option value="Containerschiff">Containerschiff</option>
-        </select>
-        <input
-          className="file-input file-input-bordered file-input-primary w-full max-w-xs"
-          type="file"
-          id="imgLink"
-          name="imgLink"
-        />
+        {/* Weitere Eingabefelder folgen... */}
+        {/* ... */}
+        {/* 'Submit'-Button zum Absenden des Formulars */}
         <input className="btn" type="submit" value="Erstellen" />
       </form>
     </div>
   );
 };
 
+// Export der Komponente für die Verwendung in anderen Teilen der Anwendung
 export default BooteAdd;
